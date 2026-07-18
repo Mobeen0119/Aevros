3f6a133d5bdb8aad0bb12ba79a171a14f53704a5
Mobeen Ahmed <amobeen652@gmail.com>
2026-07-18 22:48:34 +0500

    Use Directory and shift responsibilty form mailroom to directory

--- Networking/mailroom/mailroom.c
+++ Networking/mailroom/mailroom.c
@@ -1,4 +1,5 @@
 #include "mailroom.h"
+#include "directory.h"
 #include "../../../Lib/kprintf.h"
 #include "../../Process/task.h"
 
@@ -7,6 +8,9 @@
 #define MIN_ETHERTYPE 0x0600
 
 #define ETHERTYPE_VLAN 0x8100
+
+extern directory_entry_t __directory_start[];
+extern directory_entry_t __directory_end[];
 
 static uint32_t delivered_count;
 static uint32_t undelivered_count;
@@ -24,38 +28,27 @@ uint32_t mailroom_undelivered_count(void)
 mail_result_t mailroom_deliver(const uint8_t *frame, uint16_t length, const uint8_t our_mac[6])
 {
 
+    (void)our_mac;
     uint16_t ethertype = (uint16_t)(frame[ETHERTYPE_OFFSET] << 8) | (frame[ETHERTYPE_OFFSET + 1]);
 
-    if (ethertype < MIN_ETHERTYPE)
+    if (ethertype < MIN_ETHERTYPE || ethertype == ETHERTYPE_VLAN)
     {
-        kprintf("[Mailroom] rejected legacy length-field frame (value %d)\n", ethertype);
+        kprintf("[Mailroom] rejected legacy/VLAN-tagged frame (0x%x)\n", ethertype);
         undelivered_count++;
         return MAIL_LEGACY_REJECTED;
     }
 
-    if (ethertype == ETHERTYPE_VLAN)
+    for (directory_entry_t *e = __directory_start; e < __directory_end; e++)
     {
-        kprintf("[Mailroom] rejected VLAN-tagged frame, not supported\n");
-
-        undelivered_count++;
-        return MAIL_LEGACY_REJECTED;
+        if (e->ethertype == ethertype)
+        {
+            kprintf("[Mailroom] delivered to %s, %d bytes\n", e->name, length - 14);
+            e->handler(frame + ETH_HEADER_SIZE, (uint16_t)(length - ETH_HEADER_SIZE));
+            delivered_count++;
+            return MAIL_DELIVERED;
+        }
     }
-
-    switch (ethertype)
-    {
-    case ETHERTYPE_ARP:
-        kprintf("[Mailroom] delivered to Rolodex (ARP), %d bytes\n", length - 14);
-        delivered_count++;
-        return MAIL_DELIVERED;
-    case ETHERTYPE_IPV4:
-        kprintf("[Mailroom] delivered to Compass (IPv4), %d bytes\n", length - 14);
-        delivered_count++;
-        return MAIL_DELIVERED;
-
-    default:
-        kprintf("[Mailroom] unknown EtherType 0x%x, no department for it\n", ethertype);
-
-        undelivered_count++;
-        return MAIL_UNKNOWN_TYPE;
-    }
+    kprintf("[Mailroom] unknown EtherType 0x%x, no department for it\n", ethertype);
+    undelivered_count++;
+    return MAIL_UNKNOWN_TYPE;
 }
